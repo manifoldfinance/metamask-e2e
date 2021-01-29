47c51c9b6ef358289aa9a29b312a4d92247d9dc9
unknown <austin.akers5@gmail.com>
2021-01-28 22:47:40 -0800

    removed all logic not realated to basic actions functionality

--- src/index.js
+++ src/index.js
@@ -1,67 +1,192 @@
-
-// Modules
 import MetaMaskOnboarding from '@metamask/onboarding'
-import { isMetaMaskConnected, getAccounts } from './modules/BasicActions'
-// // Basic Actions Section
-const connectButton = document.getElementById('connectButton')
-const onboardingButton = document.getElementById('onboardingInProgressButton')
-// const getAccountsButton = document.getElementById('getAccounts')
-// const getAccountsResults = document.getElementById('getAccountsResult')
+// eslint-disable-next-line camelcase
+import { encrypt, recoverPersonalSignature, recoverTypedSignatureLegacy, recoverTypedSignature, recoverTypedSignature_v4 } from 'eth-sig-util'
+import { ethers } from 'ethers'
+import { toChecksumAddress } from 'ethereumjs-util'
+import { hstBytecode, hstAbi, piggybankBytecode, piggybankAbi } from './constants.json'
+
+let ethersProvider
+let hstFactory
+let piggybankFactory
+
+const currentUrl = new URL(window.location.href)
+const forwarderOrigin = currentUrl.hostname === 'localhost'
+  ? 'http://localhost:9010'
+  : undefined
 
 const { isMetaMaskInstalled } = MetaMaskOnboarding
 
-const currentUrl = new URL(window.location.href)
-const forwarderOrigin = currentUrl.hostname === 'localhost' ? 'http://localhost:9010' : undefined
+// Dapp Status Section
+const networkDiv = document.getElementById('network')
+const chainIdDiv = document.getElementById('chainId')
+const accountsDiv = document.getElementById('accounts')
 
-const onboarding = new MetaMaskOnboarding({ forwarderOrigin })
-let accounts
-
-onboardingButton.addEventListener('click', () => {
-  location.reload()
-})
-
-const onClickInstall = () => {
-  connectButton.style.display = 'none'
-  onboardingButton.style.display = 'block'
-  onboarding.startOnboarding()
-}
-
-const metaMaskCheck = () => {
-  if (!isMetaMaskInstalled) {
-    onboardingButton.innerText = 'Click here to install MetaMask!'
-    onboardingButton.onclick = onClickInstall
-    onboardingButton.disabled = false
-  } else if (isMetaMaskConnected()) {
-    accounts = getAccounts()
-    connectButton.innerText = 'Connected'
-    connectButton.disabled = true
-    onboardingButton.disabled = false
-  } else {
-    onboardingButton.innerText = 'Connect'
-    onboardingButton.onclick = getAccounts
-    onboardingButton.disabled = false
-  }
-}
-
+// Basic Actions Section
+const onboardButton = document.getElementById('connectButton')
+const getAccountsButton = document.getElementById('getAccounts')
+const getAccountsResults = document.getElementById('getAccountsResult')
 
 const initialize = async () => {
-  onboardingButton.style.display = 'none'
-  console.log(accounts)
-  if (!isMetaMaskInstalled()) {
-    connectButton.innerText = 'Click here to install MetaMask!'
-    connectButton.onclick = onClickInstall
-    connectButton.disabled = false
-  } else if (isMetaMaskConnected(accounts)) {
-    connectButton.disabled = true
-    if (onboarding) {
-      onboarding.stopOnboarding()
-    }
-  } else {
-    connectButton.onclick = getAccounts
-    connectButton.disabled = false
+  try {
+    // We must specify the network as 'any' for ethers to allow network changes
+    ethersProvider = new ethers.providers.Web3Provider(window.ethereum, 'any')
+    hstFactory = new ethers.ContractFactory(
+      hstAbi,
+      hstBytecode,
+      ethersProvider.getSigner(),
+    )
+    piggybankFactory = new ethers.ContractFactory(
+      piggybankAbi,
+      piggybankBytecode,
+      ethersProvider.getSigner(),
+    )
+  } catch (error) {
+    console.error(error)
   }
 
-  metaMaskCheck()
-}
+  let onboarding
+  try {
+    onboarding = new MetaMaskOnboarding({ forwarderOrigin })
+  } catch (error) {
+    console.error(error)
+  }
 
+  let accounts
+  let accountButtonsInitialized = false
+
+  const accountButtons = [
+    sendButton,
+  ]
+
+  const isMetaMaskConnected = () => accounts && accounts.length > 0
+
+  const onClickInstall = () => {
+    onboardButton.innerText = 'Onboarding in progress'
+    onboardButton.disabled = true
+    onboarding.startOnboarding()
+  }
+
+  const onClickConnect = async () => {
+    try {
+      const newAccounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      })
+      handleNewAccounts(newAccounts)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const updateButtons = () => {
+    const accountButtonsDisabled = !isMetaMaskInstalled() || !isMetaMaskConnected()
+    if (accountButtonsDisabled) {
+      for (const button of accountButtons) {
+        button.disabled = true
+      }
+      // clearTextDisplays()
+    } else {
+      // deployButton.disabled = false
+      sendButton.disabled = false
+      // createToken.disabled = false
+      // personalSign.disabled = false
+      // signTypedData.disabled = false
+      // getEncryptionKeyButton.disabled = false
+      // ethSign.disabled = false
+      // personalSign.disabled = false
+      // signTypedData.disabled = false
+      // signTypedDataV3.disabled = false
+      // signTypedDataV4.disabled = false
+    }
+
+    if (!isMetaMaskInstalled()) {
+      onboardButton.innerText = 'Click here to install MetaMask!'
+      onboardButton.onclick = onClickInstall
+      onboardButton.disabled = false
+    } else if (isMetaMaskConnected()) {
+      onboardButton.innerText = 'Connected'
+      onboardButton.disabled = true
+      if (onboarding) {
+        onboarding.stopOnboarding()
+      }
+    } else {
+      onboardButton.innerText = 'Connect'
+      onboardButton.onclick = onClickConnect
+      onboardButton.disabled = false
+    }
+  }
+
+  const initializeAccountButtons = () => {
+
+    getAccountsButton.onclick = async () => {
+      try {
+        const _accounts = await ethereum.request({
+          method: 'eth_accounts',
+        })
+        getAccountsResults.innerHTML = _accounts[0] || 'Not able to get accounts'
+      } catch (err) {
+        console.error(err)
+        getAccountsResults.innerHTML = `Error: ${err.message}`
+      }
+    }
+
+    if (accountButtonsInitialized) {
+      return
+    }
+    accountButtonsInitialized = true
+  }
+
+  function handleNewAccounts (newAccounts) {
+    accounts = newAccounts
+    accountsDiv.innerHTML = accounts
+    if (isMetaMaskConnected()) {
+      initializeAccountButtons()
+    }
+    updateButtons()
+  }
+
+  function handleNewChain (chainId) {
+    chainIdDiv.innerHTML = chainId
+  }
+
+  function handleNewNetwork (networkId) {
+    networkDiv.innerHTML = networkId
+  }
+
+  async function getNetworkAndChainId () {
+    try {
+      const chainId = await ethereum.request({
+        method: 'eth_chainId',
+      })
+      handleNewChain(chainId)
+
+      const networkId = await ethereum.request({
+        method: 'net_version',
+      })
+      handleNewNetwork(networkId)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  updateButtons()
+
+  if (isMetaMaskInstalled()) {
+
+    ethereum.autoRefreshOnNetworkChange = false
+    getNetworkAndChainId()
+
+    ethereum.on('chainChanged', handleNewChain)
+    ethereum.on('networkChanged', handleNewNetwork)
+    ethereum.on('accountsChanged', handleNewAccounts)
+
+    try {
+      const newAccounts = await ethereum.request({
+        method: 'eth_accounts',
+      })
+      handleNewAccounts(newAccounts)
+    } catch (err) {
+      console.error('Error on init when getting accounts', err)
+    }
+  }
+}
 window.addEventListener('DOMContentLoaded', initialize)
